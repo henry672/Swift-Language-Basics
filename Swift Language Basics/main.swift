627847abeef9ac681a8f3d63293fae0cede5d02e
Henry Asare <henryasare@Henrys-MacBook-Pro.local>
2017-09-08 21:12:13 -0500

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Henry Asare on 9/8/17.
+//  Copyright © 2017 Henry Asare. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
